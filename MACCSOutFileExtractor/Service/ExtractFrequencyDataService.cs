c25dc7d2b3dc91d03d75432a735d7573b61567ed
counterAttacking <ptjh0309@hanmail.net>
2021-01-14 18:09:38 +0900

    Make ExtractFrequencyDataService

---
+++ MACCSOutFileExtractor/Service/ExtractFrequencyDataService.cs
@@ -0,0 +1,31 @@
+﻿using MACCSOutFileExtractor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MACCSOutFileExtractor.Service
+{
+    public class ExtractFrequencyDataService
+    {
+        private FrequencyData[] frequencies;
+
+        private ExtractFrequencyDataService()
+        {
+
+        }
+
+        private static readonly Lazy<ExtractFrequencyDataService> extractFrequencyService = new Lazy<ExtractFrequencyDataService>(() => new ExtractFrequencyDataService());
+
+        public static ExtractFrequencyDataService GetExtractFrequencyService
+        {
+            get
+            {
+                return extractFrequencyService.Value;
+            }
+        }
+
+        public object GetFrequencies => this.frequencies.Clone();
+    }
+}
